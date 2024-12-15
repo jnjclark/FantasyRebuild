d220edc8fe36cd3763d19d5eb1d38fd312c9c185
cade510 <cademike@rocketmail.com>
2024-12-15 18:57:00 -0500

    Update DayCycle.cs

--- FantasyRebuild/Assets/Scripts/DayCycle.cs
+++ FantasyRebuild/Assets/Scripts/DayCycle.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class DayCycle : MonoBehaviour
 {
+    public int currentDay;
+    public float dayLength
+
+    private float currentTime;
+    public int dragonCount;
+    public int gameLength;
+    public Player player;
+    public GameObject dragon;
+    public Vector2 dragonStart;
+    public GameObject Grid;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,48 @@ public class DayCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //timer for length of day
+        if (currentTime <= dayLength)
+            currentTime += Time.deltaTime;
+        else
+            currentTime = 0;
+        IncreaseDay();
     }
+
+    private void IncreaseDay()
+    {
+        currentDay++;
+        Player
+        if (currentDay % dragonCount == 0)
+            TriggerDragon();
+        else if (currentDay == gameLength)
+            GameEnd();//EndGame()?
+    }
+
+    public void AddResources()
+    {
+        //wood
+        Vector2 place = Grid.RandomFreeTransform();
+        Instantiate(woodNode, place, Quaternion.identity);
+        Grid.SetOccupied(place);
+        //stone
+        place = Grid.RandomFreeTransform();
+        Instantiate(stoneNode, place, Quaternion.identity);
+        Grid.SetOccupied(place);
+        //magic
+        place = Grid.RandomFreeTransform();
+        Instantiate(magicNode, place, Quaternion.identity);
+        Grid.SetOccupied(place);
+    }
+
+    void TriggerDragon()
+    {
+        Instantiate(dragon, dragonStart, Quaternion.identity);
+    }
+
+    void EndGame()
+    {
+        player.CalculateScore();
+    }
+
 }
